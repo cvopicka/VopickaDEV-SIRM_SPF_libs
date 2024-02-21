e21b0ddfed85ce7c79f05b00dd3daded3cc75d0f
Charles E. Vopicka <chuck@vopicka.dev>
2024-02-20 19:03:35 -0700

    updated package struct for pyproject.toml

--- reset.bat
+++
@@ -1,3 +0,0 @@
-pip uninstall -y sirm_spf_libs
-python setup.py bdist_wheel
-pip install ".\dist\sirm_spf_libs-2024.217.1248a0-py3-none-any.whl"
--- src/sirm_spf_libs/__init__.py
+++ src/sirm_spf_libs/__init__.py
@@ -1,2 +1,4 @@
 from sirm_spf_libs import Config
 from sirm_spf_libs import Tools
+
+__version__ = "2024.0220.1900.alpha"
